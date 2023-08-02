f565d853dadb625adf6c282b7784440ffe6d7542
joachimhviid <joac1099@gmail.com>
2023-08-02 12:46:43 +0200

    New mending strategy

--- composables/useContentDiff.ts
+++ composables/useContentDiff.ts
@@ -1,31 +1,36 @@
 import { Diff, DIFF_DELETE, DIFF_EQUAL, DIFF_INSERT, diff_match_patch } from 'diff-match-patch'
 import { detailedDiff } from 'deep-object-diff'
+// import diff from 'node-htmldiff'
 export const useContentDiff = () => {
   const dmp = new diff_match_patch()
 
   const getContentDiff = (oldContent: string, newContent: string) => {
-    const diff = dmp.diff_main(oldContent, newContent)
-    dmp.diff_cleanupSemantic(diff)
+    const diffs = dmp.diff_main(oldContent, newContent)
+    dmp.diff_cleanupSemantic(diffs)
 
-    return wrapWithHTMLTags(mendHTMLTags(diff))
+    return wrapWithHTMLTags(mendHTMLTags(diffs))
+    // return diff(oldContent, newContent)
   }
 
   const mendHTMLTags = (diffList: Diff[]): Diff[] => {
     const resultArray: Diff[] = []
-    let isOpenEnd = false
+    const incompleteTagRegex = /<[^>]*>?$/m
+    let shouldAddToNext: [boolean, string] = [false, diffList[0][1].match(incompleteTagRegex)?.[0] ?? '']
 
     diffList.forEach(([type, text]) => {
+      const match = text.match(incompleteTagRegex)
       console.log('Mending diff', { type, text })
-      if (!text.startsWith('<') && isOpenEnd) {
-        isOpenEnd = false
-        console.log('Adding leftover bracket to', text)
-        text = '<' + text
+      if (shouldAddToNext[0]) {
+        console.log(`Adding ${shouldAddToNext[1]} to start of`, text)
+        text = shouldAddToNext[1] + text
+        shouldAddToNext = [false, '']
       }
-      if (text.endsWith('<')) {
-        console.log('Removing leftover bracket from', text)
-        text = text.slice(0, -1)
-        isOpenEnd = true
+      if (match && match[0] && !match[0].endsWith('>')) {
+        console.log(`Removing ${match[0]} from end of`, text)
+        text = text.slice(0, -match[0].length)
+        shouldAddToNext = [true, match[0]]
       }
+      console.log(`%cResult: ${text}`, 'color: green')
       resultArray.push([type, text])
     })
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useContentDiff } from '~/composables/useContentDiff'
 
-const htmlContent1 = ref<string>('')
-const htmlContent2 = ref<string>('')
+const htmlContent1 = ref<string>('<div><p>Sneakily readding a paragraph</p><div class="not-prose"  variant="0" static="true"><div class="flex space-x-2 rounded-lg shadow-sm p-3 items-center bg-white"><div class="bg-sky-100 w-12 h-12 rounded-full flex shrink-0 items-center justify-center"><svg height="26" width="26" class="text-sky-600"><path fill="currentColor" d="m12 21l-1.45-1.3q-2.525-2.275-4.175-3.925T3.75 12.812Q2.775 11.5 2.388 10.4T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.3 0 2.475.55T12 4.75q.85-1 2.025-1.55t2.475-.55q2.35 0 3.925 1.575T22 8.15q0 1.15-.388 2.25t-1.362 2.412q-.975 1.313-2.625 2.963T13.45 19.7L12 21Z"/></svg></div><div class="text-gray-700 w-full p-2"><p>icon</p></div></div></div><div class="not-prose"  variant="1" static="true"><div class="bg-white shadow-sm rounded-lg relative py-6 px-10 border border-sky-200"><div class="absolute text-5xl text-sky-600 font-serif left-4 -top-2 before:content-[&#39;&#39;] before:bg-white before:w-5 before:h-px before:absolute before:top-[7px] before:left-0 before:-z-10 z-0"> “ </div><div class="italic text-gray-700 text-center"><p>test</p></div><div class="text-sm text-sky-600 mt-4 text-center border-t pt-2"><span>Default Text</span><span> - </span><span>Default Text</span></div><div class="absolute text-5xl text-sky-600 font-serif right-6 -bottom-9 before:content-[&#39;&#39;] before:bg-white before:w-5 before:h-px before:absolute before:bottom-[35px] before:-z-10 z-0"> ” </div></div></div><div class="not-prose"  variant="0"><div class="w-full text-center font-medium text-xl mb-3">Default Title Text</div><div class="md:border md:rounded-lg md:bg-white md:border-sky-200"><table class="w-full block md:table"><thead class="hidden md:table-header-group"><tr><th class="border-r border-sky-200 w-40 bg-sky-100 rounded-tl-lg"><div class="font-medium text-sky-700 py-1 px-3 w-full">Name</div></th><th class="border-r border-sky-200 w-40 bg-sky-100"><div class="font-medium text-sky-700 py-1 px-3 w-full">Amount</div></th><th class="border-r border-sky-200 w-40 bg-sky-100"><div class="font-medium text-sky-700 py-1 px-3 w-full">Duration</div></th><th class="border-sky-200 w-40 bg-sky-100 rounded-tr-lg"><div class="font-medium text-sky-700 py-1 px-3 w-full">Rate</div></th></tr></thead></table></div></div><div class="not-prose"  variant="1" static="true"><div class="flex border space-x-2 rounded-lg p-6 items-center border-sky-200 shadow-sm"><div class="!text-gray-700 w-full"><p>hello </p></div></div></div><p>I added a bit for the article as well, changed again?</p></div>')
+const htmlContent2 = ref<string>('<div><p>Sneakily readding a paragraph</p><div class="not-prose"  variant="0" static="true"><div class="flex space-x-2 rounded-lg shadow-sm p-3 items-center bg-white"><div class="bg-sky-100 w-12 h-12 rounded-full flex shrink-0 items-center justify-center"><svg height="26" width="26" class="text-sky-600"><path fill="currentColor" d="m12 21l-1.45-1.3q-2.525-2.275-4.175-3.925T3.75 12.812Q2.775 11.5 2.388 10.4T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.3 0 2.475.55T12 4.75q.85-1 2.025-1.55t2.475-.55q2.35 0 3.925 1.575T22 8.15q0 1.15-.388 2.25t-1.362 2.412q-.975 1.313-2.625 2.963T13.45 19.7L12 21Z"/></svg></div><div class="text-gray-700 w-full p-2"><p>icon</p></div></div></div><div class="not-prose"  variant="1" static="true"><div class="bg-white shadow-sm rounded-lg relative py-6 px-10 border border-sky-200"><div class="absolute text-5xl text-sky-600 font-serif left-4 -top-2 before:content-[&#39;&#39;] before:bg-white before:w-5 before:h-px before:absolute before:top-[7px] before:left-0 before:-z-10 z-0"> “ </div><div class="italic text-gray-700 text-center"><p>test</p></div><div class="text-sm text-sky-600 mt-4 text-center border-t pt-2"><span>Default Text</span><span> - </span><span>Default Text</span></div><div class="absolute text-5xl text-sky-600 font-serif right-6 -bottom-9 before:content-[&#39;&#39;] before:bg-white before:w-5 before:h-px before:absolute before:bottom-[35px] before:-z-10 z-0"> ” </div></div></div><div class="not-prose"  variant="1" static="true"><div class="flex border space-x-2 rounded-lg p-6 items-center border-sky-200 shadow-sm"><div class="!text-gray-700 w-full"><p>hello </p></div></div></div><p>I added a bit for the article as well, changed again?</p></div>')
 
 const outputHtml = ref<string>('')
 
@@ -10,7 +10,7 @@ const { getContentDiff } = useContentDiff()
 
 watch([htmlContent1, htmlContent2], () => {
   outputHtml.value = getContentDiff(htmlContent1.value, htmlContent2.value)
-})
+}, { immediate: true })
 
 </script>
 
@@ -47,4 +47,11 @@ watch([htmlContent1, htmlContent2], () => {
 del > *, ins > * {
     background-color: inherit;
 }
+
+/*del {
+    background-color: red;
+}
+ins {
+    background-color: green;
+}*/
 </style>
